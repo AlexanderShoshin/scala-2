fbb4ea79d5b40854e9ccc2ad84c0b220b317c6f3
Alexander Shoshin <Alexander_Shoshin@epam.com>
2016-08-31 14:08:40 +0300

    write second query

--- src/main/scala/FlightsQueries.scala
+++ src/main/scala/FlightsQueries.scala
@@ -12,14 +12,25 @@ object FlightsQueries extends App {
   val carriers = sql.csvFile("/training/hive/carriers/carriers.csv", cacheTable = true)
   val flights = sql.csvFile("/training/hive/flights/2007.csv.bz2", cacheTable = true)
 
-  val flightsPerCarrierId = flights
+  val flightsPerCarrier = flights
       .groupBy("UniqueCarrier")
       .count()
-  val withDescription = flightsPerCarrierId
       .withColumnRenamed("UniqueCarrier", "Code")
       .join(carriers, "Code")
-  val flightsPerCarrier = withDescription
       .withColumnRenamed("Description", "carrier")
       .select("carrier", "count")
       .orderBy(column("count").desc)
+
+  val newYorkAirports = airports
+      .filter(lower(column("city")) === "new york")
+      .select("iata")
+      .collect()
+      .map(_.getString(0))
+
+  val flightsServedByNY = flights
+      .filter(column("Month") === 6)
+      .filter(column("Origin").isin(newYorkAirports:_*)
+          .or(column("Dest").isin(newYorkAirports:_*)))
+
+  
 }
